72760aa8066c277fb67cf30bdb7283246b9110fd
vicision <duncan.j.mclachlan@gmail.com>
2017-03-28 17:34:48 -0400

    8 spec failures remaining

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < Sinatra::Base
 
+  # enable :sessions
+  # set :session_secret, "my_application_secret"
+  #
+  # register Sinatra::ActiveRecordExtension
+
+
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
 end
---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,40 @@
+class FiguresController < ApplicationController
+
+  enable :sessions
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+  end
+
+end
